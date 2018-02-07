6020e3fdf34329d43320f746e0930e97a72cb416
Emmanuel Buah <emmanuelbuah@gmail.com>
2018-02-07 11:46:32 -0600

    Throw an error if down() function is not provided in migration setup. This closes #4

--- src/migration.ts
+++ src/migration.ts
@@ -111,6 +111,10 @@ export class Migration {
       throw new Error('Migration must supply an up function.');
     }
 
+    if (typeof migration.down !== 'function') {
+      throw new Error('Migration must supply a down function.');
+    }
+
     if (typeof migration.version !== 'number') {
       throw new Error('Migration must supply a version number.');
     }
