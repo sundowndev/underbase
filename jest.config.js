93f9b077e0dea4d4ef80985f39bf712e7733f633
sundowndev <raphael@crvx.fr>
2019-06-16 23:00:39 +0200

    feat: setup code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
