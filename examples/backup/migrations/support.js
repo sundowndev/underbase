e95b34b3abd96053fcdff37c80febe5e8d2e1688
sundowndev <raphael@crvx.fr>
2019-10-06 16:29:37 +0200

    refactor(examples): centralize populate script
    Also refactor backup example

--- examples/backup/migrations/support.js
+++ examples/backup/migrations/support.js
@@ -1,47 +1,47 @@
 const { exec } = require('child_process');
 
 module.exports = (on, { config }) => {
-  // on('connect', () => config.logger.log('Running!'));
+  on('migrate', () => new Promise((resolve, reject) => {
+        const mongodumpBin = 'mongodump';
+        const version = '1.0';
+        const backupFile = [version, `${Date.now()}.gz`].join('_');
+        const filePath = [config.migrationsDir, 'backups', backupFile].join(
+          '/',
+        );
+        const dbUrlSegments = config.db.split('/');
 
-  on('migrate', ({ config }) =>
-    new Promise((resolve, reject) => {
-      config.logger.info('Creating backup...');
+        config.logger.info('Creating backup...');
 
-      const dbUrlSegments = config.db.split('/');
-      let host;
-      let database;
+        let host;
+        let database;
 
-      if (dbUrlSegments[0] === 'mongodb:' && dbUrlSegments[1] === '') {
-        host = dbUrlSegments[2];
-        database = dbUrlSegments[3];
-      } else {
-        host = dbUrlSegments[0];
-        database = dbUrlSegments[1];
-      }
+        if (dbUrlSegments[0] === 'mongodb:' && dbUrlSegments[1] === '') {
+          host = dbUrlSegments[2];
+          database = dbUrlSegments[3];
+        } else {
+          host = dbUrlSegments[0];
+          database = dbUrlSegments[1];
+        }
 
-      const mongodumpBin = 'mongodump';
-      const version = '1.0';
-      const backupFile = [version, `${Date.now()}.gz`].join('_');
-      const filePath = [config.migrationsDir, 'backups', backupFile].join('/');
+        const cmd = [
+          mongodumpBin,
+          `--host ${host}`,
+          `--archive=${filePath}`,
+          `--gzip --db ${database}`,
+        ].join(' ');
 
-      const cmd = [
-        mongodumpBin,
-        `--host ${host}`,
-        `--archive=${filePath}`,
-        `--gzip --db ${database}`,
-      ].join(' ');
+        return exec(cmd, error => {
+          if (error) {
+            config.logger.error(
+              'An error occured while creating backup file. Cancelling.',
+            );
+            return reject(error);
+          }
 
-      return exec(cmd, error => {
-        /*if (error) {
-          config.logger.error(
-            'An error occured while creating backup file. Cancelling.',
-          );
-          return reject(error);
-        }*/
+          config.logger.success(`Backup created: ${filePath}`);
 
-        config.logger.success(`Backup created: ${filePath}`);
-
-        return resolve();
-      });
-    }));
+          return resolve();
+        });
+      }),
+  );
 };
