0ca1a7c496f169d9dcebca7b49a1f44f87f614ea
sundowndev <raphael@crvx.fr>
2019-12-14 21:04:48 +0100

    refactor(examples): add reset script

--- examples/__scripts__/test_examples.sh
+++ examples/__scripts__/test_examples.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+docker-compose up -d underbase-db
+
 SCRIPT_DIR=$(pwd)/examples/__scripts__
 EXAMPLES_DIR=$(pwd)/examples
 
@@ -7,7 +9,7 @@ EXAMPLES_DIR=$(pwd)/examples
 pip3 install -r $SCRIPT_DIR/requirements.txt
 python3 $SCRIPT_DIR/populate.py
 
-# Run migration for each examples
+# Run migration for "get-started" examples
 npm --prefix $EXAMPLES_DIR/get-started run migrations:validate
 npm --prefix $EXAMPLES_DIR/get-started run migrations:run
 npm --prefix $EXAMPLES_DIR/get-started run migrations:reset
