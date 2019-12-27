b09349bca773e4f2226ff06fe80fd1ba18e76267
Raphaël <16480203+sundowndev@users.noreply.github.com>
2019-12-27 15:06:59 +0100

    chore(examples): babel-ts
    remove mongodumpBinary from options

--- examples/babel-typescript/migrations/config.js
+++ examples/babel-typescript/migrations/config.js
@@ -2,6 +2,5 @@ module.exports = {
   db: 'mongodb://localhost:27017/underbase_example',
   migrationsDir: __dirname,
   collectionName: '_migrations',
-  mongodumpBinary: 'docker exec -it underbase_underbase-db_1 mongodump',
   logs: true,
 };
